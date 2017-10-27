f7d03052088cc7dfca583418ff5962ec64e428c3
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-27 16:23:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Song
   extend Memorable
   extend Findable
+  extend Paramable
   attr_accessor :name
   attr_reader :artist
 
@@ -16,9 +17,9 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 
   def self.all
     @@songs
